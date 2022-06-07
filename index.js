ee6c5ad89cf219c6d75cc07cd5c25fc3fca701c5
Hardik Singhal <hardiksinghal23@gmail.com>
2022-06-07 13:41:33 +0530

    make it better 5

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ try {
     console.error(error);
 }
 
-app.use(cors({ credentials:true, origin:'https://datatables-appsfactor.herokuapp.com' }));
+app.use(cors({ credentials:true, origin:'https://datatables1.netlify.app/' }));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
